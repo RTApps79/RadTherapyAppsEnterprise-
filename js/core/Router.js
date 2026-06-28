c35dbd80d84b210a1af2ac94d622ba85ea3ba217
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-28 10:14:24 -0400

    Refactor Router class constructor and methods

--- js/core/Router.js
+++ js/core/Router.js
@@ -1,14 +1,54 @@
-export default class Router{
+export default class Router {
 
-    constructor(modules){
+    constructor(moduleManager) {
 
-        this.modules=modules;
+        this.moduleManager = moduleManager;
 
     }
 
-    initialize(){
+    initialize() {
 
-        console.log("Router Ready");
+        const buttons =
+            document.querySelectorAll(
+                "#sidebar button"
+            );
+
+        buttons.forEach(button => {
+
+            button.addEventListener(
+                "click",
+                () => {
+
+                    const module =
+                        button.dataset.module;
+
+                    this.navigate(module);
+
+                }
+            );
+
+        });
+
+        this.navigate("dashboard");
+
+    }
+
+    async navigate(moduleName) {
+
+        history.pushState(
+            {},
+            "",
+            "#" + moduleName
+        );
+
+        await this.moduleManager.activate(
+            moduleName
+        );
+
+        document
+            .getElementById("statusText")
+            .innerText =
+            moduleName.toUpperCase();
 
     }
 
