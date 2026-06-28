7daeb9620e06205098de225430ec839a625fd40c
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-28 09:40:40 -0400

    Implement EventBus class for event handling

---
+++ js/core/EventBus.js
@@ -0,0 +1,27 @@
+export default class EventBus{
+
+    constructor(){
+
+        this.listeners={};
+
+    }
+
+    subscribe(event,callback){
+
+        if(!this.listeners[event])
+
+            this.listeners[event]=[];
+
+        this.listeners[event].push(callback);
+
+    }
+
+    publish(event,data){
+
+        if(!this.listeners[event]) return;
+
+        this.listeners[event].forEach(cb=>cb(data));
+
+    }
+
+}
