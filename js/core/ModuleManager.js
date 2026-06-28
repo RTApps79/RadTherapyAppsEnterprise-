f19dd12c57a23d0a61e2494ebc16183dd0e049e4
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-28 10:14:00 -0400

    Refactor ModuleManager to use Map for modules

--- js/core/ModuleManager.js
+++ js/core/ModuleManager.js
@@ -1,20 +1,72 @@
-export default class ModuleManager{
+import Logger from "./Logger.js";
 
-    constructor(store,eventBus,services){
+export default class ModuleManager {
 
-        this.modules=[];
+    constructor(store, events, services) {
 
-        this.store=store;
+        this.store = store;
+        this.events = events;
+        this.services = services;
 
-        this.events=eventBus;
+        this.modules = new Map();
 
-        this.services=services;
+        this.activeModule = null;
+
+        this.workspace =
+            document.getElementById("moduleContainer");
 
     }
 
-    async loadModules(){
+    register(module) {
 
-        console.log("Loading Enterprise Modules");
+        this.modules.set(module.name, module);
+
+        Logger.info("Registered Module : " + module.name);
+
+    }
+
+    get(name) {
+
+        return this.modules.get(name);
+
+    }
+
+    async initializeAll() {
+
+        for (const module of this.modules.values()) {
+
+            await module.initialize();
+
+        }
+
+    }
+
+    async activate(name) {
+
+        const module = this.modules.get(name);
+
+        if (!module)
+            return;
+
+        if (this.activeModule)
+            await this.activeModule.deactivate();
+
+        this.workspace.innerHTML = "";
+
+        await module.activate(this.workspace);
+
+        this.activeModule = module;
+
+        this.events.publish(
+            "moduleChanged",
+            module.name
+        );
+
+    }
+
+    getRegisteredModules() {
+
+        return [...this.modules.keys()];
 
     }
 
