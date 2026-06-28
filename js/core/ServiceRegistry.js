7d5e29f17a03cd854dfb56d9cda478fa3160aa88
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-28 10:21:59 -0400

    Enhance ServiceRegistry with additional methods
    Refactor ServiceRegistry methods and add new functionalities.

--- js/core/ServiceRegistry.js
+++ js/core/ServiceRegistry.js
@@ -2,19 +2,55 @@ export default class ServiceRegistry{
 
     constructor(){
 
-        this.services=new Map();
+        this.services=
+
+            new Map();
 
     }
 
-    register(name,obj){
+    register(name,service){
 
-        this.services.set(name,obj);
+        this.services.set(
+
+            name,
+
+            service
+
+        );
 
     }
 
     get(name){
 
+        if(!this.services.has(name))
+
+            throw new Error(
+
+                name+
+
+                " service not registered."
+
+            );
+
         return this.services.get(name);
+
+    }
+
+    exists(name){
+
+        return this.services.has(name);
+
+    }
+
+    remove(name){
+
+        this.services.delete(name);
+
+    }
+
+    clear(){
+
+        this.services.clear();
 
     }
 
