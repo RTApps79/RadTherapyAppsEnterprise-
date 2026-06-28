8d27e32dc569ffe0f8a45b6bc840124b6289a403
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-28 09:44:19 -0400

    Add Config.js with application configuration settings

---
+++ js/core/Config.js
@@ -0,0 +1,19 @@
+export default{
+
+    VERSION:"4.0 Enterprise",
+
+    BUILD:"2026",
+
+    COMPANY:"RadTherapyApps",
+
+    DICOM:true,
+
+    TPS:true,
+
+    LINAC:true,
+
+    PHYSICS:true,
+
+    AI:true
+
+}
