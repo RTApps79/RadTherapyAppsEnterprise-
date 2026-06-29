2445d79f86e30560eabfdea448608265eaec9a77
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-29 13:15:58 -0400

    Update TreatmentStatus.js

--- js/plugins/ois/components/TreatmentStatus.js
+++ js/plugins/ois/components/TreatmentStatus.js
@@ -1,1 +1,52 @@
+export default class TreatmentStatus{
 
+render(patient,container){
+
+const rx=patient.prescription ?? {};
+
+const delivered=patient.deliveredFractions ?? 0;
+
+const total=rx.fractions ?? 30;
+
+const percent=((delivered/total)*100).toFixed(1);
+
+container.innerHTML=`
+
+<div class="card">
+
+<h2>
+
+Treatment Progress
+
+</h2>
+
+<div class="progressBar">
+
+<div
+class="progressFill"
+style="width:${percent}%">
+</div>
+
+</div>
+
+<p>
+
+${delivered} / ${total}
+
+Fractions Delivered
+
+</p>
+
+<p>
+
+${percent}% Complete
+
+</p>
+
+</div>
+
+`;
+
+}
+
+}
