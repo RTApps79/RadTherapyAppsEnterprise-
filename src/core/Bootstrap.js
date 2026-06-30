db8be644ed5bac78b47d647ce51e920daef8ba41
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-29 22:02:16 -0400

    Update Bootstrap.js

--- src/core/Bootstrap.js
+++ src/core/Bootstrap.js
@@ -3,8 +3,6 @@ import TreatmentRoom3D from '../ui/widgets/TreatmentRoom3D.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('app-root');
-    
-    // Instantiate and mount the 3D Room component
     const simulator = new TreatmentRoom3D();
     root.appendChild(simulator.render());
 });
