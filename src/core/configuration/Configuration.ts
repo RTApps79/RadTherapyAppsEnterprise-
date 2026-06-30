e1771ac123ebc1e534d67a7b3d88a01dc7da8d26
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-30 10:00:45 -0400

    Create Configuration.ts

---
+++ src/core/configuration/Configuration.ts
@@ -0,0 +1,23 @@
+/*
+ * =============================================================================
+ * RTApps Enterprise
+ * =============================================================================
+ */
+
+export class Configuration {
+
+    public readonly darkMode = true;
+
+    public readonly logging = true;
+
+    public readonly autoSave = true;
+
+    public readonly enableAI = true;
+
+    public readonly enablePhysics = true;
+
+    public readonly enableDICOM = true;
+
+    public readonly enableSimulation = true;
+
+}
