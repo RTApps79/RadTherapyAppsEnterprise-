1203383167ee4343bb1907554e45724f3044f2d1
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-30 09:24:48 -0400

    Create ApplicationMetadata.ts

---
+++ apps/enterprise/src/core/ApplicationMetadata.ts
@@ -0,0 +1,24 @@
+/*
+ * RadTherapyApps Enterprise
+ *
+ * Copyright © 2026 Kevin Kindle
+ * RTApps
+ */
+
+export const ApplicationMetadata = {
+
+    product: "RadTherapyApps Enterprise",
+
+    company: "RTApps",
+
+    version: "2.0.0",
+
+    author: "Kevin Kindle",
+
+    copyright:
+        "© 2026 Kevin Kindle",
+
+    license:
+        "Proprietary"
+
+} as const;
